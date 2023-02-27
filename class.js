d1ae4aa27ee0b225bd3d2dbacabd802bcd88a309
Dmitry Bogomolov <bogomolovdv86@gmail.com>
2023-02-27 21:18:49 +0200

    заполнили массив

--- class.js
+++ class.js
@@ -14,26 +14,37 @@
         возможны (contains, starts, ends для строковых и <, =, >, <=, >= для числовых)
         добавить.и удалить
 */
-
+let items = [];
 class Goods {
-    constructor(nameGood, price, quantity, description) {
-        this.nameGood = nameGood;
+    constructor(name, price, quantity, description) {
+        this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.description = description;
+    };
+
+    getProduct(queryString) {
+        console.log(queryString);
+        return items;
     }
+
 }
 
+items.push(new Goods('Milk', 150, 4, 'Fat no less than 3%'));
+items.push(new Goods('apple', 240, 2, 'red, Spain'));
+items.push(new Goods('pear', 50, 14, 'new sort'));
+items.push(new Goods('cheese', 43, 34, 'Holland, discount 15%'));
+// Goods.items.push( new Goods('Milk', 150, 4, 'Fat no less than 3%'));
+// Goods.items.push( new Goods('Milk', 150, 4, 'Fat no less than 3%'));
+// let bag2 = new Goods('apple', 240, 2, 'red, Spain');
+// let bag3 = new Goods('pear', 50, 14, 'new sort');
+// let bag4 = new Goods('cheese', 43, 34, 'Holland, discount 15%');
+// let bag5 = new Goods('potato', 30, 12, 'Egypt, grey, 1 bag only');
+// let bag6 = new Goods('carrot', 26, 7, 'Big big VERY BIg');
+let result = Goods.prototype.getProduct('name-contais');
 
-let bag1 = new Goods('Milk', 150, 4, 'Fat no less than 3%');
-let bag2 = new Goods('apple', 240, 2, 'red, Spain');
-let bag3 = new Goods('pear', 50, 14, 'new sort');
-let bag4 = new Goods('cheese', 43, 34, 'Holland, discount 15%');
-let bag5 = new Goods('potato', 30, 12, 'Egypt, grey, 1 bag only');
-let bag6 = new Goods('carrot', 26, 7, 'Big big VERY BIg');
-
-console.log(bag1, bag2, bag3, bag4, bag5, bag6);
-let weekGoods = [];
-weekGoods.push(bag1);
-weekGoods.push(bag2);
-console.log(weekGoods);
+// console.log(bag1, bag2, bag3, bag4, bag5, bag6);
+// let weekGoods = [];
+// weekGoods.push(bag1);
+// weekGoods.push(bag2);
+// console.log(weekGoods);
